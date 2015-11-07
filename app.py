44b6e3a07ab2ba23f67805ce665f07bf9dd7305d
Damon Toumbourou <dtoumbourou@gmail.com>
2015-11-07 18:00:40 +1100

    use wit ai

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
 from flask import Flask
 from flask import request
+import json
+import wit 
 import dicttoxml
 import yweather
+import private
 app = Flask(__name__)
 
-@app.route("/pi", methods=['POST'])
+
+@app.route("/") 
 def home():
+    return "Welcome"
 
-    # default msg case
-    message = "I don't understand."
 
-    if 'temp' in request.form['Body'].lower(): 
-        message = get_temp()
-    
-    if 'weather' in request.form['Body'].lower(): 
+@app.route("/pi", methods=['POST'])
+def pi_router():
+
+    wit.init()
+
+    message = request.form['Body'] 
+
+    result = wit.text_query(message, private.WIT_API_TOKEN)
+    result = json.loads(result)
+    result = result['outcomes'][0]['intent'];
+   
+    if 'get_weather' in result:
         message = get_weather()
-        
 
     data = {'Response': {'Message': message}}
     xml = dicttoxml.dicttoxml(data, root=False)
 
     return xml, 200, {'content-type': 'text/xml'}
-
 
 
 def get_temp():
