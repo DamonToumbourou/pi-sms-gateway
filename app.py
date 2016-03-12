b0623ab7be32fe7c6edc7749284c1e4ae7ad2a94
Lex Toumbourou <lextoumbourou@gmail.com>
2016-03-12 18:32:04 +1100

    Migrate to OWM, add simple API interface and requirements file.

--- app.py
+++ app.py
@@ -1,90 +1,73 @@
-from flask import Flask
-from flask import request
 import json
-import wit 
+
+from flask import Flask, request
+import pyowm
+import wit
 import dicttoxml
-import yweather
+
 import private
+
+
 app = Flask(__name__)
 
 
-@app.route("/") 
+DEFAULT_MESSAGE = "I don't know."
+
+
+@app.route("/")
 def home():
     return "Welcome"
 
 
+@app.route("/api", methods=['GET'])
+def api():
+    """Return a message from query."""
+    text = request.args.get('query', '')
+    return json.dumps({'message': handle_query(text)})
+
+
 @app.route("/pi", methods=['POST'])
 def pi_router():
+    text = request.form['Body']
+    message = handle_query(text)
+    data = {'Response': {'Message': message}}
+    xml = dicttoxml.dicttoxml(data, root=False)
+    return xml, 200, {'content-type': 'text/xml'}
 
+
+def handle_query(text):
+    """Send a text-based query to Wit."""
     wit.init()
 
-    message = request.form['Body'] 
-    
-    response = wit.text_query(message, private.WIT_API_TOKEN)
+    message = DEFAULT_MESSAGE
+
+    response = wit.text_query(text, private.WIT_API_TOKEN)
     result = json.loads(response)
-    
-    intent = result['outcomes'][0]['intent'];
-    
+
+    intent = result['outcomes'][0]['intent']
+
     if result['outcomes'] and 'entities' in result['outcomes'][0]:
-        entity = result['outcomes'][0]['entities']    
+        entity = result['outcomes'][0]['entities']
 
     if 'get_weather' in intent:
         message = get_weather(entity)
 
-    data = {'Response': {'Message': message}}
-    xml = dicttoxml.dicttoxml(data, root=False)
-
-    return xml, 200, {'content-type': 'text/xml'}
-
-
-def get_temp():
-    return "test: 45deg c"
+    return message
 
 
 def get_weather(entity):
-        
-    location = 'Melbourne'
-    day = None;
-
-    if 'day' in entity:
-        day = entity['day'][0]['value']
+    location = 'Melbourne, Australia'
+    day = None
 
     if 'location' in entity:
         location = entity['location'][0]['value']
 
-    client = yweather.Client()
-    id = client.fetch_woeid(location)
-    
-    weather = client.fetch_weather(id, metric=True)
+    owm = pyowm.OWM(private.OWM_API_TOKEN)
+    forecast = owm.weather_at_place(location)
+    weather = forecast.get_weather()
 
-
-    if day is None:
-        desc = weather["condition"]["text"]
-        temp = weather["condition"]["temp"]
-        
-        return "The weather is {}. Current temp is {}".format(desc, temp)
-
-    
-    if get_forecast(weather, day) is None:
-        return "I can't get forecast for that day."
-
-    high, low, text = get_forecast(weather, day)
-
-    return "The weather for {} is: {}, max {}, min {}".format(day, text, high, low)
-
-
-def get_forecast(weather, day):
-    
-    forecast = weather["forecast"]
-
-    for f in forecast:
-        if f['day'] in day:
-            
-            return f['high'], f['low'], f['text'] 
-
-
-    return None
-
+    return "The weather is {0}. Current temp is {1}".format(
+        weather.get_detailed_status(), weather.get_temperature('celsius')['temp'])
 
 
 if __name__ == "__main__":
